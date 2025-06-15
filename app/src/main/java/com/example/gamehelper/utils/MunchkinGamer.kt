67807fc07c1fb1da6487c63425683a691ded3632
ShelDenis <shele1-2010@mail.ru>
2025-06-16 00:51:14 +0600

    feat: class MunckinGamer

---
+++ app/src/main/java/com/example/gamehelper/utils/MunchkinGamer.kt
@@ -0,0 +1,39 @@
+package com.example.gamehelper
+
+class MunchkinGamer {
+    val name: String
+    var gameLevel: Int
+    var wearLevel: Int
+    var summaryLevel: Int
+    var mClass: String
+
+
+    constructor(name: String, gameLevel: Int, wearLevel: Int, mClass: String = "No class") {
+        this.name = name
+        this.gameLevel = gameLevel
+        this.wearLevel = wearLevel
+        this.summaryLevel = gameLevel + wearLevel
+        this.mClass = mClass
+    }
+
+    fun changeGameLevel(n: Int) {
+        this.gameLevel += n
+        updateSumLevel()
+    }
+
+    fun changeWearLevel(n: Int) {
+        this.wearLevel += n
+        updateSumLevel()
+    }
+
+    fun changeClass(newClass: String){
+        this.mClass = newClass
+        updateSumLevel()
+    }
+
+    fun updateSumLevel() {
+        this.summaryLevel = gameLevel + wearLevel
+        if (mClass == "Воин")
+            this.summaryLevel += 1
+    }
+}
