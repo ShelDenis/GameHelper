a634343b19a537a240b2000dd9fa23a35e5241f9
ShelDenis <shele1-2010@mail.ru>
2025-07-16 22:21:52 +0600

    feat: new cards added, style optimizing

--- app/src/main/java/com/example/gamehelper/screens/TarotScreen.kt
+++ app/src/main/java/com/example/gamehelper/screens/TarotScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,12 +16,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
@@ -34,7 +35,7 @@ fun TarotScreen(navController: NavController){
 
     val nCards = remember { mutableStateOf(1) }
     val cardNums = remember { mutableStateOf(emptyList<Int>()) }
-    val n = 22
+    val n = 78
     val taro = TarotData()
 
     Box(modifier = Modifier.verticalScroll(state)) {
@@ -52,6 +53,21 @@ fun TarotScreen(navController: NavController){
                 modifier = Modifier.padding(vertical = 32.dp)
             )
 
+            val marks = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                marks.forEach { m ->
+                    Text(
+                        text = m.toString(),
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+            }
+
             Slider(
                 value = nCards.value.toFloat(),
                 onValueChange = { newValue ->
@@ -66,12 +82,18 @@ fun TarotScreen(navController: NavController){
 
             Button(
                 onClick = {
-                    cardNums.value = List(nCards.value) { Random.nextInt(0, n) }
+                    val uniqueCardNums = mutableSetOf<Int>()
+                    while (uniqueCardNums.size < nCards.value) {
+                        uniqueCardNums.add(Random.nextInt(0, n))
+                    }
+                    cardNums.value = uniqueCardNums.toList()
                 },
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Text(text = "Получить расклад")
             }
+
+            val turnList = List(nCards.value) { Random.nextBoolean() }
 
             LazyRow(
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
@@ -83,17 +105,17 @@ fun TarotScreen(navController: NavController){
                         model = address,
                         contentDescription = null,
                         modifier = Modifier.size(300.dp)
+                            .rotate(if (!turnList[index]) 180f else 0f)
                     )
                 }
             }
 
             var text_data = ""
-            for (num: Int in cardNums.value) {
-                var isStraight = Random.nextBoolean()
-                if (isStraight)
-                    text_data += taro.nameDict[num] + " --- " + taro.straightVals[num] + "\n\n"
+            for (i: Int in 0..cardNums.value.size - 1) {
+                if (turnList[i])
+                    text_data += taro.nameDict[cardNums.value[i]] + " --- " + taro.straightVals[cardNums.value[i]] + "\n\n"
                 else
-                    text_data += taro.nameDict[num] + " --- " + taro.turnedVals[num] + "\n\n"
+                    text_data += taro.nameDict[cardNums.value[i]] + " --- " + taro.turnedVals[cardNums.value[i]] + "\n\n"
             }
 
             Text(text_data)
